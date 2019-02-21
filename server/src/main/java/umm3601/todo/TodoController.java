5195ffb145b4204ab20586e42fd8e2a6d7e38b73
deglm006 <deglm006@morris.umn.edu>
2019-02-21 13:38:07 -0600

    Implement controller and database for todos Database is able to get todos by id

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,4 +1,48 @@
 package umm3601.todo;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import spark.Request;
+import spark.Response;
+
+import static umm3601.Util.buildFailJsonResponse;
+
 public class TodoController {
+
+    private final Gson gson;
+    private TodoDatabase database;
+
+    public TodoController(TodoDatabase database) {
+      gson = new Gson();
+      this.database = database;
+    }
+
+    public JsonElement getTodo(Request req, Response res) {
+      res.type("application/json");
+      String id = req.params("id");
+      Todo todo = database.getTodo(id);
+      if (todo != null) {
+
+        return gson.toJsonTree(todo);
+      } else {
+        String message = "Todo with ID " + id + " wasn't found.";
+        return buildFailJsonResponse("id", message);
+      }
+    }
+
+    /**
+     * Get a JSON response with a list of all the todos in the "database".
+     *
+     * @param req the HTTP request
+     * @param res the HTTP response
+     * @return a success JSON object containing all the todos
+     */
+    public JsonElement getTodos(Request req, Response res) {
+      res.type("application/json");
+      Todo[] todos = database.listTodos(req.queryMap().toMap());
+
+      //return buildSuccessJsonResponse("todos", gson.toJsonTree(todos));
+      return gson.toJsonTree(todos);
+    }
+
 }
--- server/src/main/java/umm3601/todo/TodoDatabase.java
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -1,4 +1,40 @@
 package umm3601.todo;
 
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
 public class TodoDatabase {
+
+  private Todo[] allTodos;
+
+  public TodoDatabase(String todoDataFile) throws IOException {
+    Gson gson = new Gson();
+    FileReader reader = new FileReader(todoDataFile);
+    allTodos = gson.fromJson(reader, Todo[].class);
+  }
+
+  Todo getTodo(String id) {
+    return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+  }
+
+  Todo[] listTodos(Map<String, String[]> queryParams) {
+    Todo[] filteredTodos = allTodos;
+
+    if(queryParams.containsKey("_id")) {
+      String targetID = queryParams.get("_id")[0];
+      filteredTodos = new Todo[0];
+      filteredTodos[0] = getTodo(targetID);
+    }
+
+    return filteredTodos;
+  }
+
+  public Todo[] filterTodosByID(Todo[] todos, String targetID) {
+    return Arrays.stream(todos).filter(x -> x._id.equals(targetID)).toArray(Todo[]::new);
+  }
+
 }
